30efa47aae3398dfb3e5a80670634aab729bc63f
gregoriusjimmy <gregoriusjimmy@gmail.com>
2020-04-11 12:47:51 +0700

    change minutes to 10

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import main
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=10)
 def timed_job():
     print("running main")
     main.updateBio()
